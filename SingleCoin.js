315c938e601fac4d4685fe29fb15e456bfc0eb1d
Jovo Simic <jovosimic@Jovos-MacBook-Air.local>
2023-08-18 19:08:24 +0200

    added back button to single coin

--- SingleCoin.js
+++ SingleCoin.js
@@ -1,12 +1,31 @@
-import { View, Text, StyleSheet, Platform, StatusBar } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SingleCoin({ route, navigation }) {
   return (
     <View style={styles.container}>
-      <SafeAreaView>
-        <Text>{JSON.stringify(route.params)}</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.backContainer}>
+          <TouchableOpacity
+            style={styles.row}
+            onPress={() => {
+              navigation.goBack();
+            }}
+          >
+            <Text style={styles.row}>Back</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.contentContainer}>
+          <Text>{JSON.stringify(route.params)}</Text>
+        </View>
       </SafeAreaView>
     </View>
   );
@@ -19,5 +38,18 @@ const styles = StyleSheet.create({
   },
   AndroidSafeArea: {
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    color: "#0E7AFE",
+    fontSize: 18,
+  },
+  backContainer: {
+    paddingVertical: 12,
+    paddingHorizontal: 22,
+  },
+  contentContainer: {
+    flex: 1,
   },
 });
