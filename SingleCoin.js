a05cd8e681a9380f5fadd9597156a60b65cfd09c
Jovo Simic <jovosimic@Jovos-MacBook-Air.local>
2023-08-18 19:35:39 +0200

    added info layer on single coin

--- SingleCoin.js
+++ SingleCoin.js
@@ -30,6 +30,74 @@ export default function SingleCoin({ route, navigation }) {
             <SvgUri width={100} height={100} uri={coin.iconUrl} />
             <Text style={styles.titleText}>{coin.name}</Text>
           </View>
+          <View style={styles.infoRow}>
+            <View style={styles.leftInfoContainer}>
+              <Text style={styles.infoTopText}>Symbol</Text>
+              <Text style={styles.infoBottomText}>{coin.symbol}</Text>
+            </View>
+            <View style={styles.infoContainer}>
+              <Text style={styles.infoTopText}>Tier</Text>
+              <Text style={styles.infoBottomText}>{coin.tier}</Text>
+            </View>
+            <View style={styles.rightInfoContainer}>
+              <Text style={styles.infoTopText}>Rank</Text>
+              <Text style={styles.infoBottomText}>{coin.rank}</Text>
+            </View>
+          </View>
+          <View style={styles.infoRow}>
+            <View style={styles.leftInfoContainer}>
+              <Text style={styles.infoTopText}>Price</Text>
+              <Text style={styles.infoBottomText}>
+                $
+                {parseFloat(coin.price).toLocaleString("en-US", {
+                  maximumFractionDigits:
+                    parseFloat(coin.price) < 2
+                      ? 6
+                      : parseFloat(coin.price) < 10
+                      ? 5
+                      : parseFloat(coin.price) < 100
+                      ? 4
+                      : parseFloat(coin.price) < 1000
+                      ? 3
+                      : parseFloat(coin.price) < 10000
+                      ? 2
+                      : 2,
+                })}
+              </Text>
+            </View>
+            <View style={styles.infoContainer}>
+              <Text style={styles.infoTopText}>BTC Price</Text>
+              <Text style={styles.infoBottomText}>
+                {parseFloat(coin.btcPrice).toLocaleString("en-US", {
+                  maximumFractionDigits:
+                    parseFloat(coin.btcPrice) < 2
+                      ? 6
+                      : parseFloat(coin.btcPrice) < 10
+                      ? 5
+                      : parseFloat(coin.btcPrice) < 100
+                      ? 4
+                      : parseFloat(coin.btcPrice) < 1000
+                      ? 3
+                      : parseFloat(coin.btcPrice) < 10000
+                      ? 2
+                      : 2,
+                })}
+              </Text>
+            </View>
+            <View style={styles.rightInfoContainer}>
+              <Text style={styles.infoTopText}>Change</Text>
+              <Text
+                style={
+                  parseFloat(coin.change) > 0
+                    ? styles.PriceUp
+                    : styles.PriceDown
+                }
+              >
+                {parseFloat(coin.change) > 0 ? "+" : ""}
+                {parseFloat(coin.change)}%
+              </Text>
+            </View>
+          </View>
           <Text>{JSON.stringify(route.params)}</Text>
         </View>
       </SafeAreaView>
@@ -70,5 +138,38 @@ const styles = StyleSheet.create({
     paddingLeft: 36,
     flexWrap: "wrap",
     flexShrink: 1,
+  },
+  infoRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingBottom: 22,
+  },
+  leftInfoContainer: {
+    gap: 2,
+  },
+  infoContainer: {
+    alignItems: "center",
+    gap: 2,
+  },
+  rightInfoContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 2,
+    minWidth: 50,
+  },
+  infoTopText: {
+    fontWeight: "700",
+  },
+  infoBottomText: {
+    fontWeight: "600",
+    color: "#B5B5B5",
+  },
+  PriceUp: {
+    color: "#52AE26",
+    fontWeight: "600",
+  },
+  PriceDown: {
+    color: "#E12323",
+    fontWeight: "600",
   },
 });
